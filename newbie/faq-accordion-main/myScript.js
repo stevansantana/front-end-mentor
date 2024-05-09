03fa741ddeedf584929f309340baa0ba62a3c73f
Stevan <stevansantanas@gmail.com>
2024-05-09 19:38:13 -0300

    Faq Accordion finalizado

---
+++ newbie/faq-accordion-main/myScript.js
@@ -0,0 +1,9 @@
+function mouseOver(element) {
+    element.style.color = "hsl(292, 16%, 49%)";
+    element.style.cursor = "pointer";
+}
+
+function mouseOut(element) {
+    element.style.color = "hsl(292, 42%, 14%)";
+    element.style.cursor = "pointer";
+}
